8ca1f405d2b0426163199f64a6d55d052a49712c
abarhub <abarhub@gmail.com>
2023-08-27 11:18:02 +0200

    ajout du fichier doc/src/klondike.js qui n'était pas versionné

---
+++ docs/src/klondike.js
@@ -0,0 +1,14 @@
+import { initialize } from "./init.js";
+import { update } from "./update.js";
+import { render } from "./render.js";
+// INIT
+initialize(true);
+// UPDATE
+// RENDER
+// ACTION
+// RUN
+requestAnimationFrame(function run() {
+    update();
+    render();
+    requestAnimationFrame(run);
+});
